a9c03b42f40ec61635c9eab67536fadc3cca8a84
iwansafr <iwansafr@gmail.com>
2018-12-19 15:05:25 +0700

    fix base url

--- one-night/meta.php
+++ one-night/meta.php
@@ -1,4 +1,12 @@
 		<?php $base_url = 'http://localhost/esg_templates/one-night/';?>
+		<?php 
+	  if(@$_SERVER['SERVER_NAME'] == 'localhost')
+	  {
+	    $base_url = 'http://localhost/esg_templates/one-night/';
+	  }else{
+	    $base_url = 'https://templates.esoftgreat.com/one-night/';
+	  }
+	  ?>
 		<title>esoftgreat - Software Development</title>
 	  <!-- Bootstrap core CSS -->
 	  <link href="<?php echo $base_url;?>vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
